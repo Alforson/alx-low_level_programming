2724210eaffb07be0f994d0066e1ded627ce333b
Alforson <asaboagyeforson@gmail.com>
2023-02-16 15:44:31 -0800

    my first C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
- * A C Program that uses a printf function
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
