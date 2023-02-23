b5501b156f44fe5ec2df691b5d75e85265bfd3c1
Alforson <asaboagyeforson@gmail.com>
2023-02-22 21:57:36 -0800

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,18 +3,17 @@
 /**
  * main - Entry point
  *
+ * Description: Prints the string "_putchar" followed by a newline
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0);
+	char str[] = "_putchar\n";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	return (0);
 }
