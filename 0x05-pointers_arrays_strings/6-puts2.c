460a0679f67f50e434404914eebfeb9d82358f55
Alforson <asaboagyeforson@gmail.com>
2023-02-27 11:37:12 -0800

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,18 +2,21 @@
 
 /**
  * puts2 - Prints every other character of a string
- *         starting with the first character
- * @str: The string to print
+ * @str: The string to be printed
+ *
+ * Return: void
  */
 
 void puts2(char *str)
 {
-        int i;
+	int len = 0;
+	int i;
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                if (i % 2 == 0)
-                        _putchar(str[i]);
-        }
-        _putchar('\n');
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
