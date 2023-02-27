2ef6483cdd18e2368d7657c045361309971fd91a
Alforson <asaboagyeforson@gmail.com>
2023-02-27 11:26:21 -0800

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,23 +3,24 @@
 /**
  * rev_string - Reverses a string
  * @s: Input string
+ *
+ * Return: void
  */
 
 void rev_string(char *s)
 {
-    int len = 0, i = 0, j = 0;
-    char temp;
+	int len = 0, i;
+	char temp;
 
-    while (s[len] != '\0')
-        len++;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-    j = len - 1;
-
-    for (i = 0; i < len / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        j--;
-    }
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
