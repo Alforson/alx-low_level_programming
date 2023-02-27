6281e50296ec9bce3b700c549a5058b52d46bdc5
Alforson <asaboagyeforson@gmail.com>
2023-02-27 14:53:33 -0800

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,17 @@
 
 void puts_half(char *str)
 {
-	int len = 0, i, n;
+	int len = 0, i;
 
 	while (str[len] != '\0')
 		len++;
 
 	if (len % 2 == 0)
-		n = len / 2;
+		i = len / 2;
 	else
-		n = (len - 1) / 2;
+		i = (len + 1) / 2;
 
-	for (i = n; i < len; i++)
+	for (; i < len; i++)
 		_putchar(str[i]);
 
 	_putchar('\n');
