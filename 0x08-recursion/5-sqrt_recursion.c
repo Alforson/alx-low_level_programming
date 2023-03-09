9a8cc69e043325b8ddf2abfa7288f8f3c2dc0586
Alforson <asaboagyeforson@gmail.com>
2023-03-08 19:26:17 -0800

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+/* Declare function prototype */
+int sqrt_helper(int n, int i);
+
 /**
  * _sqrt_recursion - Returns the natural square root of a number
  * @n: The number to find the square root of
