c8f1d02366339e73c4e62e1f27e53eaabb9c6595
Alforson <asaboagyeforson@gmail.com>
2023-03-08 18:31:59 -0800

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,15 @@
 /**
- * _strlen_recursion - returns the length of a string
- * @s: the input string
+ * _strlen_recursion - Returns the length of a string
  *
- * Return: the length of the string
+ * @s: The string to calculate the length of
+ *
+ * Return: The length of the string
  */
+
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return 0;
+	if (*s == '\0')
+		return (0);
 
-    return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
