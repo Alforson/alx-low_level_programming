1b0e17f3f828823f33cbd21c9116598f5091a919
Alforson <vboxuser@Ubuntu.20.04.myguest.virtualbox.org>
2023-06-14 22:15:36 +0000

    arg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,12 +7,9 @@
  *
  * Return: 0 on success
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	int count = 0;
-
-	while (argv[count])
-		count++;
+	int count = argc - 1;
 
 	_putchar(count + '0');
 	_putchar('\n');
