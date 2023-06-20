cb263464eb0c6c5c8efa6198ad929db24db218d6
Alforson <vboxuser@Ubuntu.20.04.myguest.virtualbox.org>
2023-06-20 03:20:55 +0000

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Structure representing a dog
+ * @name: Pointer to name of dog
+ * @age: Age of dog
+ * @owner: Pointer to owner's name
+ *
+ * Description: structure represents a dog with its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
